e768f844ed81728feeb11045f79d57e82d0b3b46
Florian Sanders <sanders.florian@gmail.com>
2024-01-27 15:45:57 +0100

    feat(typescript-lsp): add a way to select the tsserver version

--- lua/typescript-language-server-lsp.lua
+++ lua/typescript-language-server-lsp.lua
@@ -1,15 +1,32 @@
-local lspconfig  = require('lspconfig')
+local lspconfig = require("lspconfig")
+local HOME = os.getenv("HOME")
 
-require'lspconfig'.tsserver.setup{
+require("lspconfig").tsserver.setup({
   cmd = { "typescript-language-server", "--stdio" },
   filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  on_new_config = function(new_config, new_root_dir)
+    -- TODO: improve this by not using hardcoded values
+    if string.find(new_root_dir, "test") then
+      new_config.init_options = {
+        hostInfo = "neovim",
+        tsserver = {
+          path = HOME .. "/.local/lib/node_modules/typescript/lib/tsserver.js",
+        },
+      }
+    end
+  end,
   init_options = {
-    hostInfo = "neovim"
+    hostInfo = "neovim",
   },
-  root_dir = lspconfig.util.root_pattern("tsconfig-nvim.json", "tsconfig.json", "package.json", "jsconfig.json", ".git"),
+  root_dir = lspconfig.util.root_pattern(
+    "tsconfig-nvim.json",
+    "tsconfig.json",
+    "package.json",
+    "jsconfig.json",
+    ".git"
+  ),
   single_file_support = true,
-  logVerbosity = 'verbose',
   preferences = {
     importModuleSpecifierEnding = "js",
   },
-}
+})
